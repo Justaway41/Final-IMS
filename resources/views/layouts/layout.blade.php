13218ea182128bb4518406813248103654804cb0
ag-tej <tej.agrawal@deerwalk.edu.np>
2022-10-19 12:23:25 +0545

    format changes

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-</script>
-    <script src="{{ asset('js/totalTime.js') }}"></script>
-
     <title>Intern Manager</title>
 
     {{-- favicon image --}}
     <link rel="icon" href="{{ asset('images/logoOnly.png') }}" type="image/x-icon">
+
+    {{-- Hours calculator - Worklog --}}
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="{{ asset('js/totalTime.js') }}"></script>
 
     {{-- css file from public --}}
     <link href="{{ asset('css/layout.css') }}" rel="stylesheet">
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,29 +5,26 @@
         <img src="{{ asset('images/navLogo.png') }}" alt="Deerwalk Sifal School Logo">
     </div>
 
-    <div>
-        <form method="POST" class="form" action="/login" enctype="multipart/form-data">
-            @csrf
-            <div class="mb-3">
-                <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Email address"
-                    value="{{ old('email') }}">
-                @error('email')
-                    <p class="text-danger small"><small>{{ $message }}</small></p>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <input type="password" class="form-control" name="password" placeholder="Password"
-                    id="exampleInputPassword1">
-                @error('password')
-                    <p class="text-danger small"><small>{{ $message }}</small></p>
-                @enderror
-            </div>
-            <div>
-                <button type="submit">Login</button>
-            </div>
-            <div class="forgot-password">
-                <a href="/forgotpassword" class="customlink"><small>Forgot your password?</small></a>
-            </div>
-        </form>
-    </div>
+    <form method="POST" class="form" action="/login" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-3">
+            <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Email address"
+                value="{{ old('email') }}">
+            @error('email')
+                <p class="text-danger small"><small>{{ $message }}</small></p>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <input type="password" class="form-control" name="password" placeholder="Password" id="exampleInputPassword1">
+            @error('password')
+                <p class="text-danger small"><small>{{ $message }}</small></p>
+            @enderror
+        </div>
+        <div>
+            <button type="submit">Login</button>
+        </div>
+        <div class="forgot-password">
+            <a href="/forgotpassword" class="customlink"><small>Forgot your password?</small></a>
+        </div>
+    </form>
 @endsection
