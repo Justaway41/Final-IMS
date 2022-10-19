802b382f46be1d86b3f7926c434d8440b6dae8af
Justaway <kritarthasapkota999@gmail.com>
2022-10-19 15:48:10 +0545

    minor changes

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-<<<<<<< HEAD
+
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
 </script>
     <script src="{{ asset('js/totalTime.js') }}"></script>
 
->>>>>>> bd5a0823078a910602d96f613c547d222a00f166
+
     <title>Intern Manager</title>
     {{-- favicon image --}}
     <link rel="icon" href="{{ asset('images/logoOnly.png') }}" type="image/x-icon">
