53292f5f3cc5e5dc4c2f5d2e5b5b34e3bc3a884e
ag-tej <tej.agrawal@deerwalk.edu.np>
2022-10-29 17:03:31 +0545

    frontend - dashboard page, worklog page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,10 +5,50 @@
         Dashboard
     </div>
     <div class="dashboard">
-        <div class="graphs">
-            <h1>Graphs</h1>
-            <h1>Here</h1>
+
+        <!--Load the AJAX API-->
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+            // Load the Visualization API and the corechart package.
+            google.charts.load('current', {
+                'packages': ['corechart']
+            });
+
+            // Set a callback to run when the Google Visualization API is loaded.
+            google.charts.setOnLoadCallback(drawChart);
+
+            // Callback that creates and populates a data table,
+            // instantiates the pie chart, passes in the data and
+            // draws it.
+            function drawChart() {
+
+                // Create the data table.
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'Date');
+                data.addColumn('number', 'Hours Worked');
+                data.addRows([
+                    @foreach ($worklogs as $worklog)
+                        ['{{ $worklog->created_at->format('M d') }}', {{ $worklog->hours_worked }}],
+                    @endforeach
+                ]);
+
+                // Set chart options
+                var options = {
+                    'title': 'Your Working Hours:',
+                    'height': 300,
+                    'width': 400
+                };
+
+                // Instantiate and draw our chart, passing in some options.
+                var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+                chart.draw(data, options);
+            }
+        </script>
+
+        <div id="chart_div">
+
         </div>
+
         <div class="work_view">
             <h2>Total Hours: {{ $worklogs->sum('hours_worked') }}</h2>
             <section class="dashboard_scroll">
--- resources/views/worklog/index.blade.php
+++ resources/views/worklog/index.blade.php
@@ -9,38 +9,54 @@
         <form action="{{ route('Work_log.store') }}" method="POST">
             @csrf
             <section class="worklog_scroll">
+
                 <div class="mb-3">
-                    <label for="exampleFormControlTextarea1">Work</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" name="work" value="{{ old('work') }}"></textarea>
+                    <label for="workFill">Work</label>
+                    <textarea class="form-control" id="workFill" name="work" value="{{ old('work') }}"></textarea>
+                    @error('work')
+                        <p class="text-danger small"><small>{{ $message }}</small></p>
+                    @enderror
                 </div>
-                <div class="time">
+
+                <div class="timelog">
                     <div class="mb-3" id="time">
                         <label for="exampleInputEmail1">Start Time</label>
                         <input type="time" class="form-control" id="startTime" aria-describedby="emailHelp"
                             value="{{ old('start_time') }}" name="start_time">
+                        @error('start_time')
+                            <p class="text-danger small"><small>{{ $message }}</small></p>
+                        @enderror
                     </div>
                     <div class="mb-3" id="time">
                         <label for="exampleInputEmail1">End Time</label>
                         <input type="time" class="form-control" id="endTime" aria-describedby="emailHelp"
                             value="{{ old('end_time') }}" name="end_time">
+                        @error('end_time')
+                            <p class="text-danger small"><small>{{ $message }}</small></p>
+                        @enderror
                     </div>
                 </div>
-                <button onclick="timeCalculator()">Calculate Hours</button>
+
+                <button class="time_calculate" onclick="timeCalculator()">Calculate Hours</button>
+
                 <div class="mb-3">
                     <label for="exampleInputEmail1">Total Hours</label>
                     <input type="number" class="form-control" id="totalTime" aria-describedby="emailHelp"
                         value="{{ old('total_time') }}" name="hours_worked" readonly>
+                    Note: Total Hours must be between 4 to 8 hours.
+                    @error('hours_worked')
+                        <p class="text-danger small"><small>{{ $message }}</small></p>
+                    @enderror
                 </div>
+
                 <div class="form-check">
-                    <p>
-                        Note: Total Hours cannot exceed 8 hours
-                    </p>
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" required>
-                    <label class="form-check-label" for="exampleCheck1">I understand that work once submitted cannot be
-                        edited</label>
+                    <input type="checkbox" class="form-check-input" id="checkbox" required>
+                    <label class="form-check-label" for="checkbox">I understand that work once submitted cannot be
+                        edited.</label>
                 </div>
-                <div>
-                    <button type="submit" class=" m-2 btn btn-primary">Submit</button>
+
+                <div class="submit_button">
+                    <button type="submit" class="submit_work">Submit</button>
                 </div>
             </section>
         </form>
