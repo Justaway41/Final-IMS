6063538727d08a3408c320cd5bcb9b5e3a1128c4
ag-tej <tej.agrawal@deerwalk.edu.np>
2022-10-31 18:04:21 +0545

    admin pages

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en-NP">
+@extends('admin.layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <title>Intern Manager</title>
-    <link rel="icon" href="{{ asset('images/logoOnly.png') }}" type="image/x-icon">
-
-    {{-- External CSS file(admin) --}}
-    <link href="{{ asset('css/layout.css') }}" rel="stylesheet">
-
-    {{-- bootstrap css --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    {{-- bootstrap js --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
-
-    @livewireStyles
-</head>
-
-<body>
-    <livewire:box-container />
-
-    <div class="main">
-        @yield('content')
-    </div>
-
-    <livewire:footer />
-
-    @livewireScripts
-</body>
-
-</html>
+@section('files')
+@endsection
