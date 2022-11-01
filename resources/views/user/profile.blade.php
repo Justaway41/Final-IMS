68b54b4fb9b93f871075bff3d70bb15165e79402
ag-tej <tej.agrawal@deerwalk.edu.np>
2022-11-01 14:09:57 +0545

    profile border-bottom

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -7,21 +7,21 @@
         <h2 class="title2">{{ auth()->user()->role->title }}</h2>
         <div class="subdiv">
             <ul class="leftDiv">
-                <li>Department</li>
-                <li>Intern ID</li>
-                <li>Email</li>
-                <li>Mobile</li>
-                <li>Address</li>
-                <li>Contract Start Date</li>
+                <li class="border-bottom">Department</li>
+                <li class="border-bottom">Intern ID</li>
+                <li class="border-bottom">Email</li>
+                <li class="border-bottom">Mobile</li>
+                <li class="border-bottom">Address</li>
+                <li class="border-bottom">Contract Start Date</li>
                 <li>Contract End Date</li>
             </ul>
             <ul class="rightDiv">
-                <li>{{ auth()->user()->department->department_name }}</li>
-                <li>{{ auth()->user()->id }}</li>
-                <li>{{ auth()->user()->email }}</li>
-                <li>{{ auth()->user()->contact }}</li>
-                <li>{{ auth()->user()->address }}</li>
-                <li>{{ auth()->user()->contract_start_date }}</li>
+                <li class="border-bottom">{{ auth()->user()->department->department_name }}</li>
+                <li class="border-bottom">{{ auth()->user()->id }}</li>
+                <li class="border-bottom">{{ auth()->user()->email }}</li>
+                <li class="border-bottom">{{ auth()->user()->contact }}</li>
+                <li class="border-bottom">{{ auth()->user()->address }}</li>
+                <li class="border-bottom">{{ auth()->user()->contract_start_date }}</li>
                 <li>{{ auth()->user()->contract_end_date }}</li>
             </ul>
         </div>
