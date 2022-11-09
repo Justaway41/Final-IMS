9bc6a356214449075418c1f557ef38adecc15ff8
ag-tej <tej.agrawal@deerwalk.edu.np>
2022-11-09 12:16:04 +0545

    missed worklog

--- resources/views/admin/interns.blade.php
+++ resources/views/admin/interns.blade.php
@@ -1,22 +1,28 @@
 @extends('layouts.layout')
 
 @section('content')
+    <div class="head-over-display">
+        Missed Worklogs
+    </div>
 
-<table class="table bg-light">
-    <thead>
-      <tr>
-        <th scope="col">Name</th>
-        <td>Action</td>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($interns as $intern)
-      <tr>
-      <th scope="row">{{ $intern->full_name}}</th>
-      <td>
-          <a href="{{ route('Work_log.show',$intern->id) }}" class="btn btn-primary">Add Worklog</a>
-      </td>
-    </tr>
-      @endforeach
-</tbody>
+    <div class="tableBG">
+        <table class="table" style="width:40vw">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($interns as $intern)
+                    <tr class="hover">
+                        <th scope="row">{{ $intern->full_name }}</th>
+                        <td>
+                            <a href="{{ route('Work_log.show', $intern->id) }}" class="createUser">Add Worklog</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -5,11 +5,11 @@
         Settings
     </div>
     <div class="adminDash">
-        <a class="a1" href="{{ route('missedWorklog') }}">Missed Worklog</a>
-        <a class="a2" href="{{ route('users.index') }}">Interns</a> 
-        <a class="a3" href="{{ route('totalhours') }}">Total Hours</a>
-        {{-- <a class="a4" href="#">Leaves</a>
-        <a class="a5" href="#">Projects</a>
-        <a class="a6" href="#">Settings</a>  --}}
+        <a class="a6" href="{{ route('missedWorklog') }}">Missed Worklog</a>
+        <a class="a5" href="{{ route('totalhours') }}">Total Hours</a>
+        <a class="a4" href="{{ route('totalhours') }}">New Department</a>
+        <a class="a3" href="{{ route('totalhours') }}">New Role</a>
+        <a class="a1" href="#">Verify Leaves</a>
+        <a class="a2" href="#">Add Project</a>
     </div>
 @endsection
