e7d4837c6ea199e1c47b82f9b12c3c2a7701310d
pratishma09 <velv9tkiss3s@gmail.com>
2024-05-16 13:10:52 +0545

    add(softdelete)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -95,8 +95,8 @@ class UserController extends Controller
     public function destroy($id)
     {
         try{
-            User::destroy($id);
-
+            $user=User::findOrFail($id);
+            $user->delete();
             return redirect(route('users.index'))
                 ->with('success', 'User deleted successfully.');
         }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Carbon\Carbon;
 use GuzzleHttp\Psr7\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -14,7 +15,7 @@ use MilanTarami\NepaliCalendar\Facades\NepaliCalendar;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
