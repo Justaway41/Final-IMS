998e129913f625cb9e0cb5e862488869f93a061b
itdepartmentdwit <55177531+itdepartmentdwit@users.noreply.github.com>
2024-07-01 13:53:57 +0545

    Delete DBAutomation.sh

--- DBAutomation.sh
+++
@@ -1,9 +0,0 @@
-password="sakardota2"
-username="97797"
-Ip="192.168.102.136"
-
-docker exec bc595c8f81e7 mysqldump -u root intern_manager > IMS-DDL-database-$(date +"%Y-%m-%d").sql
-sshpass -p "$password" scp IMS-DDL-database-$(date +"%Y-%m-%d").sql $username@$Ip:/testing/IMS_DDL/
-
-
-rm -rf *.sql
